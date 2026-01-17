78016af0d047346cb42f0280106ccc45bf0253ba
djsarvaryanbekov <159626601+djsarvaryanbekov@users.noreply.github.com>
2026-01-17 21:01:09 +0500

    Update ShipHUD.cs

--- Assets/Game/Scripts/UI/ShipHUD.cs
+++ Assets/Game/Scripts/UI/ShipHUD.cs
@@ -13,19 +13,19 @@ public class ShipHUD : MonoBehaviour
     public TextMeshProUGUI objectiveText;
 
 
-    //public ShipController playerShip;
+    public ShipEntity playerShip;
 
-    //void Update()
-    //{
-    //    if (playerShip == null) return;
+    void Update()
+    {
+        if (playerShip == null) return;
 
-    //    // ќбновл€ем бары (значени€ должны быть нормализованы 0..1)
-    //    hullBar.fillAmount = playerShip.currentHealth / playerShip.maxHealth;
-    //    fuelBar.fillAmount = playerShip.currentFuel / playerShip.maxFuel;
+        // ќбновл€ем бары (значени€ должны быть нормализованы 0..1)
+        hullBar.fillAmount = playerShip.currentHealth / playerShip.maxHealth;
+        fuelBar.fillAmount = playerShip.currentFuel / playerShip.maxFuel;
 
-    //    // ќбновл€ем скорость (округл€ем до целого)
-    //    speedText.text = $"{Mathf.Round(playerShip.rb.velocity.magnitude)} m/s";
-    //}
+        // ќбновл€ем скорость (округл€ем до целого)
+        speedText.text = $"{Mathf.Round(playerShip.rb.velocity.magnitude)} m/s";
+    }
 
     public void SetObjective(string text)
     {
